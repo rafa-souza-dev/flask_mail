a44574937aaf4c5a33f22a759eee07016aee4b58
rafael <rafaeljosegomes25@gmail.com>
2022-05-02 18:12:30 -0300

    config structure extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
 from flask import Flask
-from flask_restful import Api
-from dynaconf import FlaskDynaconf
-from flask_cors import CORS
-from flask_mail import Mail
-from flask_jwt_extended import JWTManager
-
+from app import extensions
 
 def create_app():
     app = Flask(__name__)
-    FlaskDynaconf(app)
-    JWTManager(app)
-    CORS(app)
+    extensions.init_app(app)
+    
+    return app
